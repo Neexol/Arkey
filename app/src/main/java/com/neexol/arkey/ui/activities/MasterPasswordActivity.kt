3797dfc04a45a487258d2fbff91015319ed67457
Nikita <almostroll@yandex.ru>
2020-05-07 09:37:57 +0300

    Add splash screen

--- app/src/main/java/com/neexol/arkey/ui/activities/MasterPasswordActivity.kt
+++ app/src/main/java/com/neexol/arkey/ui/activities/MasterPasswordActivity.kt
@@ -42,6 +42,7 @@ class MasterPasswordActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_App)
         super.onCreate(savedInstanceState)
         blockScreenCapture()
         setContentView()
